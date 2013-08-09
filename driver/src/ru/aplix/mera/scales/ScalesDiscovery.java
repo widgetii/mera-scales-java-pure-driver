b10dcc6b8b2fb2bb8b398597c524d244201f9052
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2013-08-09 13:50:32 +0700

    Move the scales discovery functionality to dedicated class, which behavior could be overridden.

---
+++ driver/src/ru/aplix/mera/scales/ScalesDiscovery.java
@@ -0,0 +1,203 @@
+package ru.aplix.mera.scales;
+
+import static java.lang.Thread.currentThread;
+import static java.util.Collections.unmodifiableList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import ru.aplix.mera.scales.backend.ScalesBackendFactory;
+
+
+/**
+ * Scales discovery.
+ *
+ * <p>An instance of this class is {@link ScalesService#createScalesDiscovery()
+ * used} by scales service to find all available scales.</p>
+ *
+ * <p>By default, uses standard Java service discovery mechanism to find all
+ * available {@link ScalesBackendFactory scales backend factories}, and
+ * registers scales backends supported by them. You may wish to change this
+ * behavior by overriding the appropriate methods.</p>
+ */
+public class ScalesDiscovery {
+
+	private static final String SERVICE_FILE_NAME =
+			"META-INF/services/" + ScalesService.class.getName();
+
+	private final ScalesService service;
+	private final ArrayList<ScalesPortId> portIds = new ArrayList<>();
+
+	/**
+	 * Constructs new scales backend.
+	 *
+	 * @param service scales service to discovers backends for.
+	 */
+	public ScalesDiscovery(ScalesService service) {
+		this.service = service;
+	}
+
+	/**
+	 * Scales service to discover backends for.
+	 *
+	 * @return scales service instance passed to constructor.
+	 */
+	public final ScalesService getService() {
+		return this.service;
+	}
+
+	/**
+	 * Returns all discovered scales ports.
+	 *
+	 * <p>A {@link #discoverScales()} method is called to find them.</>
+	 *
+	 * @return available scales ports.
+	 */
+	public final List<? extends ScalesPortId> getScalesPorts() {
+		discoverScales();
+		return unmodifiableList(this.portIds);
+	}
+
+	/**
+	 * Discovers scales.
+	 *
+	 * <p>Use {@link #addPortId(ScalesProtocol, String)} and/or
+	 * {@link #addBackendFactory(ScalesBackendFactory)} to register found scales
+	 * ports and/or backend factories.</p>
+	 */
+	protected void discoverScales() {
+
+		final ClassLoader classLoader = getClassLoader();
+		final Enumeration<URL> urls;
+
+		try {
+			urls = classLoader.getResources(SERVICE_FILE_NAME);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		while (urls.hasMoreElements()) {
+			discoverPortIdsIn(classLoader, urls.nextElement());
+		}
+	}
+
+	/**
+	 * Registers the scales backends supported by the given factory.
+	 *
+	 * @param factory scales factory.
+	 */
+	protected void addBackendFactory(ScalesBackendFactory factory) {
+		try {
+
+			final ScalesProtocol protocol = factory.getScalesProtocol();
+
+			for (String deviceId : factory.scalesDeviceIds()) {
+				addPortId(protocol, deviceId);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void addPortId(ScalesProtocol protocol, String deviceId) {
+		this.portIds.add(new ScalesPortId(this.service, deviceId, protocol));
+	}
+
+	/**
+	 * Returns a class loader used to discover and load backend factories.
+	 *
+	 * @return class loader.
+	 */
+	protected ClassLoader getClassLoader() {
+
+		final ClassLoader contextClassLoader =
+				currentThread().getContextClassLoader();
+
+		if (contextClassLoader != null) {
+			return contextClassLoader;
+		}
+
+		return getClass().getClassLoader();
+	}
+
+	private void discoverPortIdsIn(ClassLoader classLoader, URL url) {
+		try {
+
+			final BufferedReader in = new BufferedReader(
+					new InputStreamReader(url.openStream(), "UTF-8"));
+
+			try {
+
+				String line;
+
+				while ((line = in.readLine()) != null) {
+
+					final String className = backendFactoryClassName(line);
+
+					if (className.isEmpty()) {
+						continue;
+					}
+
+					final ScalesBackendFactory factory =
+							createBackendFactory(classLoader, className);
+
+					if (factory == null) {
+						continue;
+					}
+
+					addBackendFactory(factory);
+				}
+			} finally {
+				in.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String backendFactoryClassName(String line) {
+
+		final int idx = line.indexOf('#');
+
+		if (idx < 0) {
+			return line;
+		}
+
+		return line.substring(0, idx).trim();
+	}
+
+	private ScalesBackendFactory createBackendFactory(
+			ClassLoader classLoader,
+			String className) {
+
+		final Class<?> klass;
+
+		try {
+			klass = Class.forName(className, true, classLoader);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		if (!ScalesBackendFactory.class.isAssignableFrom(klass)) {
+			System.err.println(
+					klass.getName() + " is not an instance of "
+					+ ScalesBackendFactory.class);
+			return null;
+		}
+
+		try {
+			return klass.asSubclass(ScalesBackendFactory.class).newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
--- driver/src/ru/aplix/mera/scales/ScalesService.java
+++ driver/src/ru/aplix/mera/scales/ScalesService.java
@@ -1,16 +1,8 @@
 package ru.aplix.mera.scales;
 
-import static java.lang.Thread.currentThread;
-import static java.util.Collections.unmodifiableList;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.*;
+import java.util.List;
 
 import ru.aplix.mera.scales.backend.ScalesBackend;
-import ru.aplix.mera.scales.backend.ScalesBackendFactory;
 
 
 /**
@@ -19,9 +11,6 @@ import ru.aplix.mera.scales.backend.ScalesBackendFactory;
  * <p>This is the main interface for accessing scales.</p>
  */
 public class ScalesService {
-
-	private static final String SERVICE_FILE_NAME =
-			"META-INF/services/" + ScalesService.class.getName();
 
 	/**
 	 * Creates a new scales service instance.
@@ -54,7 +43,17 @@ public class ScalesService {
 		if (this.sclalesPortIds == null) {
 			return this.sclalesPortIds;
 		}
-		return this.sclalesPortIds = discoverPortIds();
+		return this.sclalesPortIds = createScalesDiscovery().getScalesPorts();
+	}
+
+	/**
+	 * Creates a scales discovery instance responsible to use to find all
+	 * available scales.
+	 *
+	 * @return scales discovery instance.
+	 */
+	protected ScalesDiscovery createScalesDiscovery() {
+		return new ScalesDiscovery(this);
 	}
 
 	/**
@@ -73,135 +72,6 @@ public class ScalesService {
 				portId.getProtocol().createBackend(portId.getDeviceId());
 
 		return new ScalesPort(backend);
-	}
-
-	/**
-	 * Discovers all available scales ports.
-	 *
-	 * @return a list of port identifiers.
-	 */
-	protected List<? extends ScalesPortId> discoverPortIds() {
-
-		final ClassLoader classLoader = getClassLoader();
-		final ArrayList<ScalesPortId> portIds = new ArrayList<>();
-		final Enumeration<URL> urls;
-
-		try {
-			urls = classLoader.getResources(SERVICE_FILE_NAME);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return Collections.emptyList();
-		}
-
-		while (urls.hasMoreElements()) {
-			discoverPortIdsIn(classLoader, portIds, urls.nextElement());
-		}
-
-		return unmodifiableList(portIds);
-	}
-
-	private ClassLoader getClassLoader() {
-
-		final ClassLoader contextClassLoader =
-				currentThread().getContextClassLoader();
-
-		if (contextClassLoader != null) {
-			return contextClassLoader;
-		}
-
-		return getClass().getClassLoader();
-	}
-
-	private void discoverPortIdsIn(
-			ClassLoader classLoader,
-			ArrayList<ScalesPortId> portIds,
-			URL url) {
-		try {
-
-			final BufferedReader in = new BufferedReader(
-					new InputStreamReader(url.openStream(), "UTF-8"));
-
-			try {
-
-				String line;
-
-				while ((line = in.readLine()) != null) {
-
-					final String className = backendFactoryClassName(line);
-
-					if (className.isEmpty()) {
-						continue;
-					}
-
-					final ScalesBackendFactory factory =
-							createBackendFactory(classLoader, className);
-
-					if (factory == null) {
-						continue;
-					}
-
-					addPortIds(portIds, factory);
-				}
-			} finally {
-				in.close();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private String backendFactoryClassName(String line) {
-
-		final int idx = line.indexOf('#');
-
-		if (idx < 0) {
-			return line;
-		}
-
-		return line.substring(0, idx).trim();
-	}
-
-	private ScalesBackendFactory createBackendFactory(
-			ClassLoader classLoader,
-			String className) {
-
-		final Class<?> klass;
-
-		try {
-			klass = Class.forName(className, true, classLoader);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			return null;
-		}
-
-		if (!ScalesBackendFactory.class.isAssignableFrom(klass)) {
-			System.err.println(
-					klass.getName() + " is not an instance of "
-					+ ScalesBackendFactory.class);
-			return null;
-		}
-
-		try {
-			return klass.asSubclass(ScalesBackendFactory.class).newInstance();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	private void addPortIds(
-			ArrayList<ScalesPortId> portIds,
-			ScalesBackendFactory factory) {
-		try {
-
-			final ScalesProtocol protocol = factory.getScalesProtocol();
-
-			for (String deviceId : factory.scalesDeviceIds()) {
-				portIds.add(new ScalesPortId(this, deviceId, protocol));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 }
